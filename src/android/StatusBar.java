9e9797d76791bce12bc114ee9402fc2ec21d2ac1
Jack Davies <jackad@hotmail.co.uk>
2017-02-07 11:07:51 +0000

    Changed logic of preferences

--- src/android/StatusBar.java
+++ src/android/StatusBar.java
@@ -56,9 +56,8 @@ public class StatusBar extends CordovaPlugin {
                 // Clear flag FLAG_FORCE_NOT_FULLSCREEN which is set initially
                 // by the Cordova.
                 Window window = cordova.getActivity().getWindow();
-                String overlay = preferences.getString("StatusBarOverlaysWebView");
 
-                if(overlay == "true" && Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                if(preferences.getString("StatusBarOverlaysWebView").equals("true") && Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
                     window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
                     //Status bar color, set to whatever opacity/color you want
                     window.setStatusBarColor(Color.TRANSPARENT); 
